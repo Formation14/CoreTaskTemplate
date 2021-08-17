a3118a551eece7b1f6d1b6f2baf0c9b3afb2379e
pavel <paveltis@tut.by>
2021-08-17 09:50:03 +0300

    ver2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,12 @@
 package jm.task.core.jdbc.util;
-
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost/mydb?serverTimezone=Europe/Moscow&useSSL=false";
-    private static final String USERNAME = "Pavel";
-    private static final String PASSWORD = "1111";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
 
     public Util() {
